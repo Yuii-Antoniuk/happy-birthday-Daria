7b9419c65c43522b8710b45ed53aea7edd4ef76d
Yuii-Antoniuk <u983405@gmail.com>
2025-07-10 16:41:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function clc() {
+var body = document.getElementById("body")
+var bnt = prompt("ввести код");
+var number = 5793
+if(bnt == number) {
+    body.style.background = "white"
+}
+
+else {
+    alert("false")
+}
+}
